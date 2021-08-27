a8099bb0d440481ffb51cb7a31692224160047ef
Torsten <info@helmichs.net>
2021-08-27 13:48:21 +0200

    Initial commit.

---
+++ Usagi.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Usagi.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
